5a2c71a701a5205c20dddca48e4b4d3bc3f5f577
mmmeyers <marsi286@gmail.com>
2015-11-20 15:11:23 -0500

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,6 @@
+class Post
+#belongs to Author. Set setter & getter methods for author.
+  attr_accessor :author, :title
+
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,5 @@
+class Song
+  #belongs to an Artist; set setter & getter methods for artist
+  attr_accessor :artist, :title
+
+end
