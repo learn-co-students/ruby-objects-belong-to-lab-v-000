c6254a0c704ad3ae9bd1c67a600e52bf13f3cab9
corya0687 <corya0687@gmail.com>
2016-01-08 19:47:08 -0500

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,9 @@
+class Post
+	attr_accessor :title, :author
+end
+
+
+new_post = Post.new
+sallie = Author.new
+
+new_post.author = sallie
---
+++ lib/song.rb
@@ -0,0 +1,8 @@
+class Song
+	attr_accessor :title, :artist
+end
+
+eight_bars = Song.new
+kazzmatik = Artist.new
+
+eight_bars.artist = kazzmatik
