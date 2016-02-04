c209dfb26a47f762619976771b2079f49376775a
evanscloud <dijkstrascloud@gmail.com>
2016-02-04 16:54:49 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,7 @@
+class Artist
+  attr_accessor :name
+
+  def artist
+    @name = name
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,11 @@
+class Song
+  attr_accessor :title, :artist
+
+  def initialize
+    @title = title
+  end
+
+  def artist=(artist)
+    @artist = artist
+  end
+end
