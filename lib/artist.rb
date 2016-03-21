a8c79b55e05d4c481bf295f018c6363ff81ce006
PeteAJ <peter.jacobson@gmail.com>
2016-03-21 16:52:16 -0400

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,7 +5,6 @@ class Artist
     @name = name
     @genre = genre
   end
-  
 Artist.name
 end
 
---
+++ lib/author.rb
@@ -0,0 +1,9 @@
+class Artist
+  attr_accessor :name, :kind
+ 
+  def initialize(name, kind) 
+    @name = name
+    @kind = kind
+  end
+
+end
