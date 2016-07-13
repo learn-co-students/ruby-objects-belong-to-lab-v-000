6548fa12a5794a0eefddf7638391744776022df5
Onyekachi <onye@smmaven.com>
2016-07-13 08:00:48 -0700

    Done

---
+++ lib/artist.rb
@@ -0,0 +1,21 @@
+class Artist
+	attr_accessor :name
+
+	def initialize
+		
+	end
+end
+
+class Author
+	attr_accessor :name, :title
+end
+
+class Post
+	attr_accessor :title, :author
+
+
+end
+
+class Song
+	attr_accessor :title, :artist
+end
