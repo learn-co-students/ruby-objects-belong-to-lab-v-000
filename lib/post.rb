e28c1c02df97744cc151e150abb0960042a48e05
Teresa Grosch <tgrosch91@gmail.com>
2016-11-12 01:09:11 +0000

    Done.

--- lib/post.rb
+++ lib/post.rb
@@ -1,6 +1,10 @@
+
 class Post
 
 attr_accessor :title, :author
-
+def initialize
+  author = Author.new
+  @author = author
+end
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -2,5 +2,9 @@ class Song
 
 attr_accessor :title, :artist
 
+def initialize
+	artist = Artist.new
+    @artist = artist
+end
 
 end
