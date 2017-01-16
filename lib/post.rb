97a49f840a81c1afb520fee23aa4f78f231665fa
Terry-Thompson <terry.lyn.thompson@gmail.com>
2017-01-16 10:33:44 -0600

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,9 @@
+class Post
+  attr_accessor :title, :author 
+
+  def initialize(title = nil, author = nil)
+    @title = title
+  end# of initialize
+
+
+end# of post
---
+++ lib/song.rb
@@ -0,0 +1,11 @@
+class Song
+attr_accessor :name, :title, :artist
+
+  def initialize(name = nil, artist = nil)
+    @name = name
+    @title = title
+    @artist = artist
+  end# of initialize
+
+
+end# of class
