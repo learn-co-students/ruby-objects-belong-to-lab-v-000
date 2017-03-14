fa4aea1524372bae09d969b53070f0cf032d618b
Hima Chitalia <hima_chhag@yahoo.com>
2017-03-14 19:49:27 +0000

    Done

---
+++ lib/song.rb
@@ -0,0 +1,18 @@
+class Song
+
+  attr_accessor :title, :artist
+
+  def initialize
+    @title = title
+    end
+
+  end
+
+  class Artist
+    attr_accessor :name
+
+    def initialize
+      @name = name
+    end
+
+  end
