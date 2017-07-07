d671d4586dcd88acfe84d7ad8c7ab4297f803875
Robert Peckham <peckham.rob@gmail.com>
2017-07-07 18:33:47 +0000

    Done.

---
+++ lib/example.rb
@@ -0,0 +1,24 @@
+class Song
+
+  attr_accessor :title, :artist
+
+  def initialize(title)
+    @title = title
+  end
+
+end
+
+class Artist
+  attr_accessor :name, :genre
+
+  def initialize(name, genre)
+    @name = name
+    @genre = genre
+  end
+
+end
+
+drake = Artist.new("Drake", "rap")
+hotline_bling = Song.new("Hotline Bling")
+
+hotline_bling.artist = drake # Note: 'drake' is an object, not a string
