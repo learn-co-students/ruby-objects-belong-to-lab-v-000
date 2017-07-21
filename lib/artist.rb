43538035730e1c831080b9d46cc5f42ea341b7db
Isabel Vazquez <12ivazquez@gmail.com>
2017-07-21 23:24:56 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,8 @@
+class Artist #has a name
+  attr_accessor :name
+
+  def initialize
+    @name = name
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,11 @@
+class Song
+  attr_accessor :title, :artist
+
+  def initialize
+    @title = title
+  end
+end
+
+#has a title
+#belongs to an artist
+#knows the name of the artist it belongs to
