432d3d19f90af95e49fa0e990a7d249844317754
Alec Alba <alecgalba@gmail.com>
2017-07-28 15:48:39 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,9 @@
+class Artist
+  attr_accessor :name, :genre
+
+  def initialze(name, genre)
+    @name = name
+    @genre = genre
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,7 @@
+class Song
+  attr_accessor :title, :artist
+
+  def initialze(title)
+    @title = title
+  end
+end
