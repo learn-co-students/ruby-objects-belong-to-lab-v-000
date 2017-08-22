e39c7654fd1b5bd79797153824febe9aa37f333d
github username <github email address>
2017-08-21 20:14:07 -0700

    Done.

--- lib/post.rb
+++ lib/post.rb
@@ -1,6 +1,9 @@
 class Post
   attr_accessor :title, :author
-  def self.author
-    self.author = Author.new
-  end
 end
+
+# There is nothing that requires that the artist attribute be
+# filled with an instance of the Artist class.
+# This is an internal contract that you must keep.
+# As the developer you must make sure that you only put Artist
+# instances in there!
