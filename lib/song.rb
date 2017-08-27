254ee40b23fb04cd4c1416de9ecc235a9b80fcc4
Chris Rudolph <chrisjames.r@gmail.com>
2017-08-27 19:57:18 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,12 @@
+class Song
+    attr_accessor :title, :artist
+
+    def initialize(title=nil)
+      @title = title
+    end
+
+    def self.artist
+      @artist = artist.name
+    end
+
+end # End of Class
