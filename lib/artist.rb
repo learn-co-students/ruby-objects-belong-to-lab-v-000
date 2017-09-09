a1646ecbcd2ba4cd9d47cea5ba59a3a1f012c470
Hunter Kleindl <hkleindl@gmail.com>
2017-09-09 22:13:07 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,9 @@
+class Artist
+
+  attr_accessor :name
+
+  def initialized(name)
+    @name = name
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,9 @@
+class Song
+
+  attr_accessor :title, :artist
+
+  def initialized(title)
+    @title = title
+  end
+
+end
