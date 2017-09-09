13f0cc49491632be9f565bb3d9e73e4f0b71f3cb
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-09 17:48:57 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,12 @@
+class Post
+  attr_accessor :title
+
+  def author=(name)
+    @name = name
+  end
+
+  def author
+    @name
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,13 @@
+class Song
+
+  attr_accessor :title
+
+  def artist
+    @name
+  end
+
+  def artist=(name)
+    @name = name
+  end
+
+end
