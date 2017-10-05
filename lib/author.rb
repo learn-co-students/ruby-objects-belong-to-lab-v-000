fd0ce112e79e7a61b2dae834e20c2f4fc1a06b84
Lara Sanders <larasanders@gmail.com>
2017-10-05 18:07:39 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,8 @@
+class Author
+  attr_accessor :name, :author
+
+  def initialize
+    @name = name
+    @author = author
+  end
+end
---
+++ lib/post.rb
@@ -0,0 +1,9 @@
+class Post
+  attr_accessor :name, :author, :title
+
+  def initialize
+    @name = name
+    @author = author
+    @title = title
+  end
+end
