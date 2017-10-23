54df4bc5f04e1d6f5e8de59131c3abaff4e8d593
Kathleen Connell <kathleenconnell11@gmail.com>
2017-10-23 18:23:14 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,10 @@
+#=> songs should have a title and belong to an artists
+# song should be able to tell you name of artist
+
+class Song
+  attr_accessor :title, :artist
+
+  def initialize
+  
+  end
+end
