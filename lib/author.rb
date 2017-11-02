94666ce7704e4e3c6d39727a825d67a6b6bc590a
mmb16 <mmb583@nyu.edu>
2017-11-02 15:01:34 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,8 @@
+class Author
+  attr_accessor :name
+
+  def initilize(name)
+    @name = name
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,9 @@
+
+class Song
+  attr_accessor :title, :artist
+
+  def initilize(title)
+    @title = title
+  end
+
+end
