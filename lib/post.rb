d1355306b0e0b093fb67be697a349c9f9fa0109f
Joshua Needham <joshuabneedham@gmail.com>
2017-11-10 19:19:40 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,23 @@
+class Post
+  attr_accessor :title, :author
+
+  def initialize
+    @title = title
+  end
+
+  def title=(title)
+    @title = title
+  end
+
+  def title
+    @title
+  end
+
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,23 @@
+class Song
+  attr_accessor :title, :artist
+
+  def initialize
+    @title = title
+  end
+
+  def title=(title)
+    @title = title
+  end
+
+  def title
+    @title
+  end
+
+  def artist=(artist)
+    @artist = artist
+  end
+
+  def artist
+    @artist
+  end
+end
