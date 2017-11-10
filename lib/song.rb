8e7d97f8e87a4b587e283faac0def43cc11ee87a
Joshua Needham <joshuabneedham@gmail.com>
2017-11-10 21:42:54 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -3,21 +3,22 @@ class Song
 
   def initialize
     @title = title
-  end
-
-  def title=(title)
-    @title = title
-  end
-
-  def title
-    @title
-  end
-
-  def artist=(artist)
     @artist = artist
   end
-
-  def artist
-    @artist
-  end
+  #
+  # def title=(title)
+  #   @title = title
+  # end
+  #
+  # def title
+  #   @title
+  # end
+  #
+  # def artist=(artist)
+  #   @artist = artist
+  # end
+  #
+  # def artist
+  #   @artist
+  # end
 end
