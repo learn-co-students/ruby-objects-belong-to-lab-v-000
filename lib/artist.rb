32031835f05f6c0463311f4e59486b1d47028d35
Bryan Hooper <bryan@lindsayfarm.net>
2017-11-21 09:38:26 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,17 @@
+# Artist class
+#
+# A song belongs to an artist...remember that.
+#
+
+
+class Artist
+
+  attr_accessor :name 
+
+  def initilize(name = "")
+    @name = name
+  end
+
+end
+
+
---
+++ lib/song.rb
@@ -0,0 +1,17 @@
+# Song class
+#
+# A song belong to an artist....remember that!
+#
+
+
+class Song
+
+  attr_accessor :title, :artist
+
+  def initilize(title)
+    @title = title
+  end
+
+end
+
+
