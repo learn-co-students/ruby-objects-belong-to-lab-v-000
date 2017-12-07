f414eb5ad8b97a6e563918b8b10efd6af7382de6
Jessica Alonso <alonso.jessicam@gmail.com>
2017-12-07 16:46:24 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,9 @@
+class Post
+
+  attr_accessor :title, :author
+
+  def post(title)
+    @title = title
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,9 @@
+class Song
+
+  attr_accessor :title, :artist
+
+  def song(title)
+    @title = title
+  end
+
+end
