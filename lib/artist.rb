3e62caebaeaa8a97e86165f87d309411b9872317
JOSELY CASTRO <joselycas@gmail.com>
2017-12-11 16:06:41 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,16 @@
+class Song
+  attr_accessor :title
+
+  def initialize(title)
+    @title = title
+  end
+end
+
+
+class Artist
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
