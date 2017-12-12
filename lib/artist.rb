64c39eac65d610ef8a08af6d639cb89fe8e96a80
Frank McCallion <keith@sensortech.com>
2017-12-12 13:04:46 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,10 @@
+require 'pry'
+
+class Artist
+  attr_accessor :name
+
+  def initialize
+    @name = name
+    #binding.pry
+  end
+end
---
+++ lib/author.rb
@@ -0,0 +1,10 @@
+require 'pry'
+
+class Author
+  attr_accessor :name
+
+  def initialize
+    @name = name
+    #binding.pry
+  end
+end
