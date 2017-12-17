4659b43513a50c6deab326602d040513f0a266ac
Jennifer Stoos <jstoos@bitlaw.com>
2017-12-17 00:05:35 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,25 @@
+class Post
+
+attr_accessor :title, :author
+
+  def initialize
+    @title = title
+    @author = author
+  end
+
+end
+
+
+class Author
+
+attr_accessor :name
+
+  def initialize
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+end
