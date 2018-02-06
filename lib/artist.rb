f8905f6c9d2fc4ec3771bdfe4fa41afef658ab1b
nptravis <nptravis@yahoo.com>
2018-02-06 11:24:15 +0700

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,16 @@
+class Artist
+	attr_accessor :name, :genre
+
+	# def initialize
+	# 	@name=name
+	# 	@genre=genre
+	# end
+
+	# def name
+	# 	@name
+	# end
+
+end
+
+
+
---
+++ lib/song.rb
@@ -0,0 +1,15 @@
+class Song
+	attr_accessor :artist, :title
+
+	# def initialize(name)
+	# 	@name = name
+	# end
+
+	# def artist(artist)
+	# 	@artist = artist
+	# end
+
+end
+
+song.artist.name
+  # => "Beyonce"
