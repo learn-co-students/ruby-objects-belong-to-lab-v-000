4173911ac9f867a938e688efa0529531191ca5e6
Brandon Brasson <rocksong622@gmail.com>
2018-02-23 22:11:06 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,13 @@
+class Artist
+  attr_accessor :Beyonce
+
+  def initialize Beyonce
+    @name = name
+  end
+end
+
+
+```ruby
+song.artist.name
+  # => "Beyonce"
+```
---
+++ lib/author.rb
@@ -0,0 +1,9 @@
+
+
+      class Author
+      attr_accessor :UncleBob
+
+     def initialize(name)
+        @name = name
+      end
+    end
