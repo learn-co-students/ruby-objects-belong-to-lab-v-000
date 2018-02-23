8c96aa59f284dd783fe15d3e69f4af14fa13d345
Brandon Brasson <rocksong622@gmail.com>
2018-02-23 22:22:04 +0000

    Done.

--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -1,9 +1,9 @@
 require "spec_helper"
 
-describe "Artist" do 
+ puts describe "Artist" do
   it "has a name" do
     artist = Artist.new
-    artist.name = "Beyonce" 
+    artist.name = "Beyonce"
     expect(artist.name).to eq("Beyonce")
   end
 end
