6787f785a560742b19b768e5c54c2d4b9fd1935c
emi kojima <emi8emi@gmail.com>
2018-02-28 23:51:58 +0000

    Done.

--- lib/post.rb
+++ lib/post.rb
@@ -1,7 +1,4 @@
 class Post 
   attr_accessor :title, :author
-  def post (title, author)
-    @title = title
-    @author = author
-  end
+ 
 end 
--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,4 @@
 class Song 
   attr_accessor :title, :artist 
-  def song (title, artist)
-    @title = title
-    @artist - artist
-  end 
+  
 end 
