2bc0005533746bd8c9daa77047f317bfca471aa0
Natalie Halawani <hala1224@gmail.com>
2018-03-08 03:14:04 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,11 @@
 class Artist
-  attr_accessor :name
-   def initialize(name)
-     @name = name
-   end
+  # extend Song::ClassMethods
+  attr_accessor :artist, :name
+  def self.create
+    artist = Artist.new
+  end
+  def self.name(artist_name)
+    artist = Artist.create
+    artist.name = artist
+  end
  end
