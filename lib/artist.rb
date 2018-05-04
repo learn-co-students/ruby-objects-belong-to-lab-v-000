df85c22322718666b4791831dd97ae4b32356a6d
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-05-04 22:14:51 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,6 @@
+class Artist
+  attr_accessor :name, :genre #no need for initialization check the spec.
+  #def initialize
+
+  #end
+end
--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -1,9 +1,10 @@
 require "spec_helper"
 
-describe "Artist" do 
+describe "Artist" do
   it "has a name" do
-    artist = Artist.new
-    artist.name = "Beyonce" 
+    artist = Artist.new #artist is a new instance of Artist class and calls
+    #no parameters.
+    artist.name = "Beyonce"
     expect(artist.name).to eq("Beyonce")
   end
 end
