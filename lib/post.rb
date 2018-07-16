ca022467b91259754fd5a4deb5ddd1da579182de
Mehwish Aziz <mehwisha@msn.com>
2018-07-16 18:14:48 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,12 @@
+class Post
+attr_accessor :title, :artist, :name, :author
+def initialize
+@title = title
+@artist = artist
+@name = name
+@author = author
+
+end
+
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,12 @@
+class Song
+
+  attr_accessor :title, :artist, :name
+
+  def initialize
+    @name = name
+    @title = title
+    @artist = artist
+
+  end
+
+end
