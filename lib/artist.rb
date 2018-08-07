d05e6a8ea86264d912f9840cf5afae1a8ce60476
Anna Dybas <a.m.dybas@gmail.com>
2018-08-07 23:19:51 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,9 @@
+class Artist
+  attr_accessor :name
+end
+
+
+# Artists should have a name.
+# Songs should have a title and belong to an artist. A song should be able to tell you the name of its artist:
+# song.artist.name
+#   # => "Beyonce"
