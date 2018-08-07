97f4351dc14eae932efc53078d5877374b04b73b
jgurowsky@gmail.com <jgurowsky@gmail.com>
2018-08-07 21:31:28 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,11 @@
+class Song
+  attr_accessor :title, :artist
+end
+
+# imagine_dragons = Artist.new
+# imagine_dragons.name = "Imagine Dragons"
+# radioactive = Song.new
+# radioactive.artist = imagine_dragons
+
+# radioactive.artist.name
+# => "Imagine Dragons"
