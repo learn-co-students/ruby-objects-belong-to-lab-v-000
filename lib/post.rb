4637a5a5f0ad6d1d21c6af6530189d2e200d2d6f
Angie Kassab <anasab12@gmail.com>
2018-08-11 13:51:25 +0000

    Done.

--- lib/post.rb
+++ lib/post.rb
@@ -2,8 +2,8 @@ class Post
   attr_accessor :title, :author
   
   def author=(author)
-    author.class
-    if author.is_a?(Author)
+    author.class #this will return Author
+    if author.is_a?(Author) #to check on the type of something
       @author = author
     else 
       raise TypeError, "Instance of Author expected, got #{author.class}"
