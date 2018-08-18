a3cba1583ec29c3de41759ab5b5751b66a1b2442
Austin Powell <austinpowell58@gmail.com>
2018-08-18 18:56:39 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,8 @@
+class Artist
+  attr_accessor :name
+  
+  def initialize(artist_name = "James")
+    @name = artist_name
+  end 
+  
+end
---
+++ lib/song.rb
@@ -0,0 +1,8 @@
+class Song
+  attr_accessor :title, :artist
+  
+  def initialize(song_title = "Holy Song")
+    @title = song_title
+  end 
+  
+end 
