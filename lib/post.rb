0900529942cbab1ac44ebe884ab086d3816e8c07
Guy Bryant <guy.s.bryant@gmail.com>
2018-09-30 02:18:17 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,11 @@
+class Post
+  attr_accessor :title, :author
+  
+  def author=(author)
+    @author = author
+  end
+  
+  def author
+    @author
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,11 @@
+class Song
+  attr_accessor :title, :artist
+  
+  def artist=(artist)
+    @artist = artist
+  end
+  
+  def artist
+    @artist
+  end
+end
