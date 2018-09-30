afa4787cf974c048c8f2cd169d686364b541c2b1
Guy Bryant <guy.s.bryant@gmail.com>
2018-09-30 02:19:33 +0000

    Done.

--- lib/post.rb
+++ lib/post.rb
@@ -1,11 +1,3 @@
 class Post
   attr_accessor :title, :author
-  
-  def author=(author)
-    @author = author
-  end
-  
-  def author
-    @author
-  end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,3 @@
 class Song
   attr_accessor :title, :artist
-  
-  def artist=(artist)
-    @artist = artist
-  end
-  
-  def artist
-    @artist
-  end
 end
