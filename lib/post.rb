40f3632e411e3faf0fcca4f1976c7419d53de08d
Paul Fell-Cinoman <swellpf@gmail.com>
2018-11-01 18:37:33 +0000

    Done.

--- lib/post.rb
+++ lib/post.rb
@@ -1,9 +1,9 @@
 class Post
-  attr_accessor :title :author
+  attr_accessor :title, :author
   
   def initialize
     @title=title
   end
-  post.author=author
-  post.author.name
+ # post.author=author
+ # post.author.name
 end
