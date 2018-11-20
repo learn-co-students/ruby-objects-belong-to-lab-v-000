47e03594ee7998b127e75e7b84098bc241bbc303
Kristen Robertson <kristen77@gmail.com>
2018-11-20 20:09:26 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,12 @@
+class Post 
+  attr_accessor :title, :author
+  
+  def title
+    @title 
+  end
+  
+  def author
+    @author
+  end
+  
+end 
---
+++ lib/song.rb
@@ -0,0 +1,14 @@
+class Song  
+  attr_accessor :title, :artist 
+  
+  def title
+    @title 
+  end
+  
+  def artist
+    @artist
+  end
+  
+end 
+
+
